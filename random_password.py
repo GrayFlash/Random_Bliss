75d0acfe9604c459dff2793da636d0f051192a45
GrayFlash <gk74533@gmail.com>
2020-07-02 15:53:07 +0530

    Degrees of Relation Ship

---
+++ random_password.py
@@ -0,0 +1,14 @@
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "[]}{<>#\/|!@$%^&*()_-/*+.,?:;'/"
+
+all = lower + upper + numbers + symbols
+
+length = int(input("Enter the length of password: "))
+
+password = "".join(random.sample(all, length))
+
+print(password)
